e3fa31a0bbf1e9ddc2e6c01f1e634776f7e6438a
Mamuka Sarkisyan <mamukinto2@gmail.com>
2020-11-21 21:33:51 +0400

    implemented UI :)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,20 +1,21 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -25,65 +26,105 @@ public class Main extends Application {
     private static final double CELL_SIZE = 5;
     private static final double RADIUS = 700;
     private static final double SQUARE_SIDE = RADIUS;
-    private static final double CIRCLE_X = WIDTH/2 - RADIUS/2;
-    private static final double CIRCLE_Y = HEIGHT/2 - RADIUS/2;
-    private static final double SQUARE_X = WIDTH/2 - SQUARE_SIDE/2;
-    private static final double SQUARE_Y = HEIGHT/2 - SQUARE_SIDE/2;
+    private static final double CIRCLE_X = WIDTH / 2 - RADIUS / 2;
+    private static final double CIRCLE_Y = HEIGHT / 2 - RADIUS / 2;
+    private static final double SQUARE_X = WIDTH / 2 - SQUARE_SIDE / 2;
+    private static final double SQUARE_Y = HEIGHT / 2 - SQUARE_SIDE / 2;
+    private static final double DOT_RADIUS = SQUARE_SIDE/100;
 
 
-    private static List<Dot> dotsInSquare = new ArrayList<>();
-    private static List<Dot> dotsInCircle = new ArrayList<>();
+    private static final int MIN_SPEED = 0;
+    private static final int MAX_SPEED = 100;
+    private static final int START_SPEED = 50;
+    private static int SPEED = 50;
 
 
 
-    private static final Canvas canvas = new Canvas(WIDTH,HEIGHT);
+    private static int dotsInSquare = 0;
+    private static int dotsInCircle = 0;
+    private static double approxPI = 0;
+
+    private static boolean isSimOn = false;
+    private static Timer timer = new Timer();
+
+
+    private static final Canvas canvas = new Canvas(WIDTH, HEIGHT);
     private static final GraphicsContext g = canvas.getGraphicsContext2D();
+
+
+    private static Label dotsInSquareLabel = Style.styledLabel("Dots in square: " + dotsInSquare);
+    private static Label dotsInCircleLabel = Style.styledLabel("Dots in circle: " + dotsInCircle);
+    private static Label approximation = Style.styledLabel("Approximation of PI: " + approxPI);
+    private static Label realPI = Style.styledLabel("Real PI: " + Math.PI);
+
 
     @Override
     public void start(Stage stage) {
         BorderPane root = new BorderPane();
 
 
-
         // side menu
         VBox sideMenu = new VBox();
-        sideMenu.setPrefWidth(200);
+        sideMenu.setPrefWidth(350);
         sideMenu.setPrefHeight(stage.getHeight());
-        sideMenu.setAlignment(Pos.CENTER);
+        sideMenu.setAlignment(Pos.BASELINE_LEFT);
         sideMenu.setSpacing(25);
+        sideMenu.setPadding(new Insets(200, 25, 0, 25));
 
-        Button drawButton = new Button("Draw");
-        drawButton.setOnAction(click -> draw());
+        Button drawButton = new Button("Redraw");
+        drawButton.setOnAction(click -> {
+            dotsInCircle = 0;
+            dotsInSquare = 0;
+            draw();
+        });
         Style.styleButton(drawButton);
 
         Button startButton = new Button("Start simulation");
-        startButton.setOnAction(click -> startSim());
+        startButton.setOnAction(click -> {
+            if (!isSimOn) {
+                startButton.setText("Stop simulation");
+                isSimOn = true;
+                startSim();
+            } else {
+                startButton.setText("Start simulation");
+                isSimOn = false;
+                timer.cancel();
+            }
+
+        });
         Style.styleButton(startButton);
+
+
+        Label speedLabel = Style.styledLabel("Adjust dots spawning speed:");
+
+        Slider speedSlider = new Slider(MIN_SPEED, MAX_SPEED, START_SPEED);
+        speedSlider.setMinorTickCount(5);
+        speedSlider.setMajorTickUnit(10);
+        speedSlider.setBlockIncrement(2);
+        speedSlider.setShowTickLabels(true);
+        speedSlider.setShowTickMarks(true);
+        speedSlider.setOnMouseClicked(dragDone -> {
+            SPEED = 1001 - ((int) speedSlider.getValue() * 10);
+            System.out.println(SPEED = 1001 - ((int) speedSlider.getValue() * 10));
+        });
 
 
 
 
         sideMenu.getChildren().add(drawButton);
         sideMenu.getChildren().add(startButton);
-
-
-
-
-
-
-
-
-
+        sideMenu.getChildren().add(speedLabel);
+        sideMenu.getChildren().add(speedSlider);
+        sideMenu.getChildren().addAll(dotsInSquareLabel,dotsInCircleLabel,approximation,realPI);
 
         root.setRight(sideMenu);
         root.setCenter(canvas);
-        Scene scene = new Scene(root,WIDTH + 200,HEIGHT);
+        Scene scene = new Scene(root, WIDTH + 400, HEIGHT);
         stage.setScene(scene);
         root.setStyle("-fx-background-color: #2b2b2b");
 
 
-
-
+        draw();
         stage.show();
 
 
@@ -124,58 +165,59 @@ public class Main extends Application {
 
 
     private static void draw() {
-        g.clearRect(0,0,WIDTH,HEIGHT);
+        g.clearRect(0, 0, WIDTH, HEIGHT);
 
         g.setLineWidth(15);
 
 
-        g.strokeRect(SQUARE_X,SQUARE_Y,SQUARE_SIDE,SQUARE_SIDE);
+        g.strokeRect(SQUARE_X, SQUARE_Y, SQUARE_SIDE, SQUARE_SIDE);
         g.setFill(Color.web("#A3C9A8"));
-        g.fillRect(SQUARE_X,SQUARE_Y,SQUARE_SIDE,SQUARE_SIDE);
+        g.fillRect(SQUARE_X, SQUARE_Y, SQUARE_SIDE, SQUARE_SIDE);
 
 
-
-        g.strokeOval(CIRCLE_X,CIRCLE_Y, RADIUS, RADIUS);
+        g.strokeOval(CIRCLE_X, CIRCLE_Y, RADIUS, RADIUS);
         g.setFill(Color.web("#69A297"));
-        g.fillOval(CIRCLE_X,CIRCLE_Y, RADIUS, RADIUS);
+        g.fillOval(CIRCLE_X, CIRCLE_Y, RADIUS, RADIUS);
 
     }
 
     private static void startSim() {
         class Refresh extends TimerTask {
             public void run() {
-                double x = CIRCLE_X + Math.random()*RADIUS;
-                double y = CIRCLE_Y + Math.random()*RADIUS;
-                Dot dot = new Dot(x,y);
-
+                double x = SQUARE_X + Math.random() * SQUARE_SIDE - DOT_RADIUS/2;
+                double y = CIRCLE_Y + Math.random() * SQUARE_SIDE - DOT_RADIUS/2;
 
                 double centerX = CIRCLE_X + RADIUS;
                 double centerY = CIRCLE_Y + RADIUS;
 
-                if  ((SQUARE_X < x && x < SQUARE_X + SQUARE_SIDE) && (SQUARE_Y < y && y < SQUARE_Y + SQUARE_SIDE)) {
-                    dotsInSquare.add(dot);
-                    if (Math.sqrt(Math.pow(x - centerX,2) + Math.pow(y - centerY,2)) < RADIUS) {
-                        dotsInCircle.add(dot);
+                if ((SQUARE_X < x && x < SQUARE_X + SQUARE_SIDE) && (SQUARE_Y < y && y < SQUARE_Y + SQUARE_SIDE)) {
+                    dotsInSquare++;
+                    if (Math.sqrt(Math.pow(x - centerX, 2) + Math.pow(y - centerY, 2)) < RADIUS) {
+                        dotsInCircle++;
                     }
                 }
 
 
                 g.setFill(Color.web("black"));
-                g.fillOval(x,y,SQUARE_SIDE/50,SQUARE_SIDE/50);
+                g.fillOval(x, y, DOT_RADIUS, DOT_RADIUS);
 
-                double approxPI = (double) 4 * dotsInCircle.size()/dotsInSquare.size();
+                approxPI = (double) 4 * dotsInCircle / dotsInSquare;
 
-                System.out.println("CIRLCE: " + dotsInCircle.size() + "    SQUARE: " + dotsInSquare.size() + "     APPROX: " + approxPI);
+                System.out.println("CIRLCE: " + dotsInCircle + "    SQUARE: " + dotsInSquare + "     APPROX: " + approxPI);
+
+                Platform.runLater(() -> {
+                    dotsInCircleLabel.setText("Dots in square: " + dotsInSquare);
+                    dotsInSquareLabel.setText("Dots in circle: " + dotsInCircle);
+                    approximation.setText("Approximation of PI: " + approxPI);
+                });
 
             }
         }
 
-        Timer timer = new Timer();
         TimerTask refresh = new Refresh();
-        timer.schedule(refresh, 0, 17);
+        timer = new Timer();
+        timer.schedule(refresh, 0, SPEED);
     }
-
-
 
 
     public static void main(String[] args) {
--- src/sample/Style.java
+++ src/sample/Style.java
@@ -1,11 +1,21 @@
 package sample;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
 
 public class Style {
     public static void styleButton(Button button) {
-        button.setPrefWidth(150);
-        button.setPrefHeight(100);
-        button.setStyle("-fx-font-size: 18; -fx-padding: 4 12 4 12; -fx-height: 10;");
+        button.setStyle("-fx-font-size: 18; -fx-padding: 4 12 4 12; -fx-height: 10px;");
+    }
+    public static Label styledLabel(String s) {
+        Label label = new Label(s);
+        label.setAlignment(Pos.BASELINE_LEFT);
+        label.setTextAlignment(TextAlignment.LEFT);
+        label.setTextFill(Color.web("#9b9b9b"));
+        label.setStyle("-fx-font-size: 16px;");
+        return label;
     }
 }
